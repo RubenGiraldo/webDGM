629bca375f735669d52b6e6b02dad367b00a3210
RubenGiraldo <39750960+RubenGiraldo@users.noreply.github.com>
2018-06-11 18:25:31 -0500

    Add files via upload

--- enviarCorreo.php
+++ enviarCorreo.php
@@ -1,6 +1,6 @@
 <?php
 
-header("content-type: text/html;charset='utf-8'")
+header("content-type: text/html;charset='utf-8'");
 
 $nombre = htmlentities($_POST['nombre']);
 $email = htmlentities($_POST['email']);
@@ -8,16 +8,16 @@ $mensaje = htmlentities($_POST['mensaje']);
 $direccion_ip = $_SERVER['REMOTE_ADDR'];
 
 $header = 'From: '.$email." \n";
-$header.= "X-Mailer: PHP/".phpversion()." \n";
-$header.= "content-type: text/plain";
+$header .= "X-Mailer: PHP/".phpversion()." \n";
+$header .= "content-type: text/plain";
 $mensaje = "Este mensaje fue enviado" .$nombre." \n";
 $mensaje = "Su email es".$email." \n";
-$mensaje = "Su mensaje es".$mensaje." \n";
-$mensaje.= "desde la IP" .$direccion_ip ." \n";
+$mensaje .= "Su mensaje es".$mensaje." \n";
+$mensaje .= "desde la IP" .$direccion_ip ." \n";
 $para = "rubengiraldomurcia@gmail.com";
 $asunto = "Mensaje del sitio web";
 
-if(mail($para,$asunto,utf8_decode($mensaje),$header))
+if(mail($para,$asunto, utf8_decode($mensaje),$header)){
 	echo "Mensaje enviado gracias por comunicarse con nosotros";
 }
 
